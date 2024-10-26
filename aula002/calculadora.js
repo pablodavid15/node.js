8aee90bb215f0c7b69461dde485e708682b60b8d
Pablo David <144298884+pablodavid15@users.noreply.github.com>
2024-10-25 21:15:19 -0300

    Add files via upload

---
+++ aula002/calculadora.js
@@ -0,0 +1,6 @@
+var SomaFunc = require('../aula002/funcoes/soma')
+var Subfunc = require('../aula002/funcoes/subtracao')
+let Multifunc = require('../aula002/funcoes/multiplica')
+console.log(SomaFunc(6, 4))
+console.log(Subfunc(5, 3))
+console.log(Multifunc(10, 20))
