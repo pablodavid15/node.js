ae51ef424ef02a25bde6b57af40d0270ece4c67b
Pablo David <144298884+pablodavid15@users.noreply.github.com>
2024-10-29 21:05:36 -0300

    Add files via upload

---
+++ test/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res){
+    res.send('Ola, bem-vindo ao meu site.')
+});
+
+app.get('/incio', function(req, res){
+    res.sendFile(`${__dirname}/html/index.html`)
+})
+
+app.listen(8182, function() {
+    console.log('Servidor rodando na url http://localhost:8182')
+});
